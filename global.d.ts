bbeea18e348a24dea4c729da62c8a8c39ff6a763
Santiago Haspert <santiago76945@yahoo.es>
2025-07-04 10:41:00 -0300

    before css big changes

--- global.d.ts
+++ global.d.ts
@@ -1,17 +1,17 @@
-// global.d.ts
+// // global.d.ts
 
-import * as React from "react";
+// import * as React from "react";
 
-declare namespace JSX {
-    interface IntrinsicElements {
-        style: React.DetailedHTMLProps<
-            React.StyleHTMLAttributes<HTMLStyleElement>,
-            HTMLStyleElement
-        > & {
-            jsx?: boolean;
-            global?: boolean;
-        };
-    }
-}
+// declare namespace JSX {
+//     interface IntrinsicElements {
+//         style: React.DetailedHTMLProps<
+//             React.StyleHTMLAttributes<HTMLStyleElement>,
+//             HTMLStyleElement
+//         > & {
+//             jsx?: boolean;
+//             global?: boolean;
+//         };
+//     }
+// }
 
-declare module '*.module.css';
+// declare module '*.module.css';
